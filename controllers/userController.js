56f0b5ed79b52dcbd2c800c9814cac1695737408
karticbavoria <kartic188@gmail.com>
2025-12-03 10:55:02 -0700

    update

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+const filePath = require('./database.json');
+const {readData, writeData}= require('./utils/files.js');
+
+//controller file. controller files handle user input and application logic 
+
+async function createUser(req, res){
+try{
+    //need 
+
+
+}catch(error){
+
+}
+
+}
